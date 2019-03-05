a4661393f4628910160842c3f928a2f2fe2dccd2
Antoine <antoine.drian@laposte.net>
2019-03-05 09:19:41 +0100

    :star: feat: add RequestedAuthnContext Comparison Type parameter
    test: add test for check the option comparisonType

--- test/tests.js
+++ test/tests.js
@@ -1405,6 +1405,19 @@ describe( 'passport-saml /', function() {
 
         done();
       });
+
+      it('should check the value of the option `comparisonType`', function(done) {
+        var samlObjBadComparisonType = new SAML({ comparisonType: 'bad_value' });
+        should.equal(samlObjBadComparisonType.options.comparisonType, 'exact', ['the default value of the option `comparisonType` must be exact']);
+
+        var validComparisonTypes = ['exact','minimum','maximum','better'], samlObjValidComparisonType;
+        validComparisonTypes.forEach(function(comparisonType) {
+            samlObjValidComparisonType = new SAML( {comparisonType: comparisonType} );
+            should.equal(samlObjValidComparisonType.options.comparisonType, comparisonType);
+        });
+        done();
+      });
+
     });
 
     describe( 'InResponseTo validation checks /', function(){
