c5ceaca215591a7ce1009661ef48cdc64beba624
Rob Gijsens <32069491+rob-gijsens@users.noreply.github.com>
2020-11-19 23:52:47 +0100

    fix(typing): multi saml stratey export (#503)
    Co-authored-by: Rob Gijsens <rob.gijsens@ivengi.com>

--- src/passport-saml/index.ts
+++ src/passport-saml/index.ts
@@ -1,6 +1,7 @@
 import type { CacheItem, CacheProvider} from './inmemory-cache-provider';
 import { SAML } from './saml';
 import Strategy = require('./strategy');
+import MultiSamlStrategy = require('./multiSamlStrategy');
 import type { Profile, VerifiedCallback, VerifyWithRequest, VerifyWithoutRequest } from './types';
 
-export { SAML, Strategy, CacheItem, CacheProvider, Profile, VerifiedCallback, VerifyWithRequest, VerifyWithoutRequest };
+export { SAML, Strategy, MultiSamlStrategy, CacheItem, CacheProvider, Profile, VerifiedCallback, VerifyWithRequest, VerifyWithoutRequest };
