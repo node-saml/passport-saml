c61cbad96c742ebde36f2b4fff2408675e6f30b6
Gabriel Fournier <gab@gfournier.ca>
2020-12-21 17:06:31 -0500

    Reexport SamlConfig type to solve a regression in consumer packages (#516)
    * fix: Reexport SamlConfig type to solve a regression in consumer packages
    
    * doc: Typo in template

--- src/passport-saml/index.ts
+++ src/passport-saml/index.ts
@@ -2,6 +2,6 @@ import type { CacheItem, CacheProvider} from './inmemory-cache-provider';
 import { SAML } from './saml';
 import Strategy = require('./strategy');
 import MultiSamlStrategy = require('./multiSamlStrategy');
-import type { Profile, VerifiedCallback, VerifyWithRequest, VerifyWithoutRequest } from './types';
+import type { Profile, SamlConfig, VerifiedCallback, VerifyWithRequest, VerifyWithoutRequest } from './types';
 
-export { SAML, Strategy, MultiSamlStrategy, CacheItem, CacheProvider, Profile, VerifiedCallback, VerifyWithRequest, VerifyWithoutRequest };
+export { SAML, Strategy, MultiSamlStrategy, CacheItem, CacheProvider, Profile, SamlConfig, VerifiedCallback, VerifyWithRequest, VerifyWithoutRequest };
