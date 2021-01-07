9323c094f022a49b1c9fabf0ec1f72fbc9dad44c
andrii@short.io <andrii@short.io>
2021-01-07 16:25:55 +0000

    add more tags to PRs, remove tags without releases

--- .grenrc.js
+++ .grenrc.js
@@ -2,6 +2,7 @@ module.exports = {
     "dataSource": "prs",
     "prefix": "",
     "onlyMilestones": false,
+    "ignoreTagsWith": ["v0.32.0", "0.0.3"],
     "groupBy": false,
     "changelogFilename": "CHANGELOG.md"
 }
