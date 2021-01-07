e3d9b8620dc6ea26949c5e6330a4b5a33cb6f710
Chris Barth <chrisjbarth@hotmail.com>
2021-01-07 15:53:50 -0500

    Add code to use a ISO date. Set CHANGELOG generating script.

--- .grenrc.js
+++ .grenrc.js
@@ -1,8 +1,21 @@
 module.exports = {
-    "dataSource": "prs",
-    "prefix": "",
-    "onlyMilestones": false,
-    "ignoreTagsWith": ["v0.32.0", "0.0.3"],
-    "groupBy": false,
-    "changelogFilename": "CHANGELOG.md"
-}
+  dataSource: "commits",
+  prefix: "",
+  onlyMilestones: false,
+  ignoreTagsWith: ["v0.32.0", "0.0.3"],
+  tags: "all",
+  groupBy: false,
+  changelogFilename: "CHANGELOG.md",
+  template: {
+    release: function (placeholders, body) {
+      let dateParts = placeholders.date.split("/");
+      let placeholdersDate = new Date(
+        Number(dateParts[2]),
+        Number(dateParts[1]) - 1,
+        Number(dateParts[0])
+      );
+      let isoDateString = placeholdersDate.toISOString().split("T")[0];
+      return `## ${placeholders.release} (${isoDateString})\n${placeholders.body}`;
+    },
+  },
+};
