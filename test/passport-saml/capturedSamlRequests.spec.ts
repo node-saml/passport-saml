ef34b699917e15846eaa71127df5abb4da6df4aa
kdhttps <kdhttps@gmail.com>
2021-06-08 15:08:48 +0530

    test(capturedsamlrequests.Spec.Ts): add test for extensions element in saml request test https://github.com/node-saml/passport-saml/issues/602

--- test/passport-saml/capturedSamlRequests.spec.ts
+++ test/passport-saml/capturedSamlRequests.spec.ts
@@ -859,6 +859,73 @@ const capturedSamlRequestChecks: SamlCheck[] = [
       },
     },
   },
+  {
+    name: "Config with Extensions",
+    config: {
+      cert: FAKE_CERT,
+      extensions: {
+        "md:RequestedAttribute": {
+          "@isRequired": "true",
+          "@Name": "Lastname",
+        },
+        vetuma: {
+          "@xmlns": "urn:vetuma:SAML:2.0:extensions",
+          LG: {
+            "#text": "sv",
+          },
+        },
+      },
+    },
+    result: {
+      "samlp:AuthnRequest": {
+        $: {
+          "xmlns:samlp": "urn:oasis:names:tc:SAML:2.0:protocol",
+          Version: "2.0",
+          ProtocolBinding: "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST",
+          AssertionConsumerServiceURL: "http://localhost:3033/login",
+          Destination: "https://wwwexampleIdp.com/saml",
+        },
+        "saml:Issuer": [
+          { _: "onelogin_saml", $: { "xmlns:saml": "urn:oasis:names:tc:SAML:2.0:assertion" } },
+        ],
+        "samlp:NameIDPolicy": [
+          {
+            $: {
+              "xmlns:samlp": "urn:oasis:names:tc:SAML:2.0:protocol",
+              Format: "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress",
+              AllowCreate: "true",
+            },
+          },
+        ],
+        "samlp:RequestedAuthnContext": [
+          {
+            $: { "xmlns:samlp": "urn:oasis:names:tc:SAML:2.0:protocol", Comparison: "exact" },
+            "saml:AuthnContextClassRef": [
+              {
+                _: "urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport",
+                $: { "xmlns:saml": "urn:oasis:names:tc:SAML:2.0:assertion" },
+              },
+            ],
+          },
+        ],
+        "samlp:Extensions": [
+          {
+            "md:RequestedAttribute": [
+              {
+                $: { isRequired: "true", Name: "Lastname" },
+              },
+            ],
+            vetuma: [
+              {
+                $: { xmlns: "urn:vetuma:SAML:2.0:extensions" },
+                LG: ["sv"],
+              },
+            ],
+          },
+        ],
+      },
+    },
+  },
 ];
 
 export const logoutChecks: CapturedCheck[] = [
