383ae881176b7087994acc480aff8e9f3b730e63
Tom Giddings <tom.giddings@jisc.ac.uk>
2021-07-28 11:30:14 +0100

    Patch algorithm definitions
    To match node-saml master and spec.

--- src/node-saml/saml.ts
+++ src/node-saml/saml.ts
@@ -1402,8 +1402,8 @@ class SAML {
           },
           EncryptionMethod: [
             // this should be the set that the xmlenc library supports
-            { "@Algorithm": "http://www.w3.org/2001/04/xmlenc#aes256-gcm" },
-            { "@Algorithm": "http://www.w3.org/2001/04/xmlenc#aes128-gcm" },
+            { "@Algorithm": "http://www.w3.org/2009/xmlenc11#aes256-gcm" },
+            { "@Algorithm": "http://www.w3.org/2009/xmlenc11#aes128-gcm" },
             { "@Algorithm": "http://www.w3.org/2001/04/xmlenc#aes256-cbc" },
             { "@Algorithm": "http://www.w3.org/2001/04/xmlenc#aes128-cbc" },
           ],
