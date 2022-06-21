be4402a85fc513470942953d229159a82137e9fc
Chris Barth <chrisjbarth@hotmail.com>
2022-06-21 16:31:38 -0400

    Update dependencies (#696)

--- test/capturedSamlRequests.spec.ts
+++ test/capturedSamlRequests.spec.ts
@@ -1041,7 +1041,7 @@ describe("captured SAML requests /", function () {
       const strategy = new SamlStrategy(
         config,
         function (_profile: Profile | null, done: VerifiedCallback) {
-          // for singon
+          // for signon
           if (_profile) {
             profile = _profile;
             done(null, profile);
